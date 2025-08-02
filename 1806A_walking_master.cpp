efd6daada7287ca2431f4db354d9e49a6789406d
venkat8352 <teja.b8352@gmail.com>
2025-08-02 11:33:00 +0530

    Create 1806A_walking_master.cpp

---
+++ 1806A_walking_master.cpp
@@ -0,0 +1,29 @@
+// 1806A Walking Master
+// https://codeforces.com/problemset/problem/1806/A
+// https://www.tle-eliminators.com/cp-sheet  rating 800, problem 
+// 02/08/2025
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while(t--) {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    //(a,b) -> N x [(++, ++) or (--, )] -> (c, d)
+    int m = 0;
+    int x = a;
+    int y = b;
+    //try incrementing y to reach d
+    //try decrementing x to reach c
+    while(y < d) { x++; y++; m++; }
+    while(x > c) { x--;      m++; }
+    if((x == c) && (y == d))
+      cout << m << endl;
+    else
+      cout << -1 << endl;
+  }
+  return 0;
+}

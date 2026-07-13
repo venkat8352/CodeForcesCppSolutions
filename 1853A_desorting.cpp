70340428dfc0704372dc44754ea09898cb47108f
venkat8352 <teja.b8352@gmail.com>
2026-07-13 19:32:59 +0530

    Add solution for 1853A Desorting problem
    Implement solution for Codeforces problem 1853A Desorting.

---
+++ 1853A_desorting.cpp
@@ -0,0 +1,38 @@
+// 1853A Desorting
+// https://codeforces.com/problemset/problem/1853/A
+// 12/07/2026
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while(t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto &v: a) {
+      cin >> v;
+    }
+    bool sorted = true;
+    int diff = (1<<30)-1;
+    for(int i = 1; i < n; i++) {
+      if(a[i-1] > a[i]) {
+        sorted = false;
+      }
+      else {
+        if((a[i] - a[i-1]) < diff) diff = a[i]-a[i-1];
+      }
+    }
+    //cout << "--- Answer ---" << endl;
+    if(!sorted) {
+      cout << 0 << endl;
+    }
+    else {
+      if(diff == 0) cout << 1 << endl;
+      else cout << (diff/2)+1 << endl;
+    }
+  }
+  return 0;
+}
